56b30217fde248615e8c28df6cb92a4fa477e267
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2024-03-19 20:07:39 +0700

    chore: added some fs files

--- src/fat32.c
+++ src/fat32.c
@@ -1,8 +1,10 @@
 #include <stdint.h>
 #include <stdbool.h>
-#include <stddef.h>
+#include <stddef.h>>
 #include "header/stdlib/string.h"
 #include "header/filesystem/fat32.h"
+
+static struct FAT32DriverState fat32driver_state;
 
 const uint8_t fs_signature[BLOCK_SIZE] = {
     'C', 'o', 'u', 'r', 's', 'e', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',  ' ',
@@ -13,3 +15,47 @@ const uint8_t fs_signature[BLOCK_SIZE] = {
     [BLOCK_SIZE-2] = 'O',
     [BLOCK_SIZE-1] = 'k',
 };
+
+uint32_t cluster_to_lba(uint32_t cluster){
+    return(cluster * CLUSTER_SIZE);
+}
+
+void init_directory_table(struct FAT32DirectoryTable *dir_table, char *name, uint32_t parent_dir_cluster){
+    // OPTIONAL
+}
+
+bool is_empty_storage(void){
+    // Waiting...
+}
+
+void create_fat32(void){
+    // Waiting...
+}
+
+void initialize_filesystem_fat32(void){
+    // Waiting...
+}
+
+void write_clusters(const void *ptr, uint32_t cluster_number, uint8_t cluster_count){
+    // OPTIONAL
+}
+
+void read_clusters(void *ptr, uint32_t cluster_number, uint8_t cluster_count){
+    // OPTIONAL
+}
+
+int8_t read_directory(struct FAT32DriverRequest request){
+    // OPTIONAL
+}
+
+int8_t read(struct FAT32DriverRequest request){
+    // OPTIONAL
+}
+
+int8_t write(struct FAT32DriverRequest request){
+    // OPTIONAL
+}
+
+int8_t delete(struct FAT32DriverRequest request){
+    // OPTIONAL
+}
