29d0c76bbb57479b9da11a352c522dd26d2c86ad
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2024-05-10 22:07:26 +0700

    fix: modified idt

--- src/idt.c
+++ src/idt.c
@@ -1,4 +1,5 @@
 #include "header/cpu/gdt.h"
+#include <stdint.h>
 #include "header/cpu/idt.h"
 
 struct InterruptDescriptorTable interrupt_descriptor_table;
@@ -7,8 +8,11 @@ struct IDTR _idt_idtr = {
 };
 
 void initialize_idt(void) {
-    for(int i = 0; i < ISR_STUB_TABLE_LIMIT; i++)
-        set_interrupt_gate(i, isr_stub_table[i], GDT_KERNEL_CODE_SEGMENT_SELECTOR, 0);
+    for(int i = 0; i < ISR_STUB_TABLE_LIMIT; i++) {
+        uint8_t priv = (i == 0x30) ? (0x3) : (0);
+        set_interrupt_gate(i, isr_stub_table[i], GDT_KERNEL_CODE_SEGMENT_SELECTOR, priv);
+    }
+
     __asm__ volatile("lidt %0" : : "m"(_idt_idtr));
     __asm__ volatile("sti");
 }
