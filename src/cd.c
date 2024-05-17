53a87fabc4bf8e36a51ea79d249a93470b7d8439
FedrianzD <fedrianzd@gmail.com>
2024-05-17 17:34:34 +0700

    cd

--- src/cd.c
+++ src/cd.c
@@ -1,9 +1,11 @@
 #include <stdint.h>
 #include "../header/stdlib/string.h"
 #include "../header/user/user-shell.h"
-void cd(char* argv[], int argc){
+#include "../header/user/cd.h"
+int cd(char* argv[], int argc){
     if(argc > 2){
-        put("cd: too many arguments\n", 0xF);
+        put("Error: too many arguments\n", 0xF);
+        return 1;
     }else{
         uint32_t search_directory_number = ROOT_CLUSTER_NUMBER;
         char* pathTemp = argv[1];
@@ -22,15 +24,15 @@ void cd(char* argv[], int argc){
             int entry = findDirEntryIndex(path[index]);
             // tidak ketemu
             if(entry == -1){
-                print("cd: no such directory", 0xF);
+                print("Error: no such directory", 0xF);
                 print(pathTemp, 0xF);
                 print("\n", 0xF);
-                return;
+                return 2;
             }
             // entry di directory table bukan directory
             if(dirTable.table[entry].attribute != ATTR_SUBDIRECTORY){
-                print("cd: not a directory\n", 0xF);
-                return;
+                print("Error: not a directory\n", 0xF);
+                return 3;
             }
             cluster_number = (int)(dirTable.table[index].cluster_high << 16) + dirTable.table[index].cluster_low;
             // get directory table
@@ -40,4 +42,5 @@ void cd(char* argv[], int argc){
         search_directory_number = cluster_number;
 
     }
+    return 0;
 }
