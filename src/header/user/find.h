b91dc19fc8f25c2fc546bbff9dca41acb1ed14ae
FedrianzD <fedrianzd@gmail.com>
2024-05-17 19:23:18 +0700

    Update find.h

--- src/header/user/find.h
+++ src/header/user/find.h
@@ -1,6 +1,9 @@
 #ifndef _USER_FIND
 #define _USER_FIND
 
-void find(char* argv[], int argc);
+#include <stdint.h>
 
+void find(char* argv[], int argc);
+void printPathFromList(char* pathList[], int lenPath);
+void findHelper(char* item, uint32_t curDirCluster, bool visited[], char* pathList[], int lenPath, bool *found);
 #endif
